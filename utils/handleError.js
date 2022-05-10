90730d83c194d93ab7a731bb3241c2b3bb78a65e
emi <bell881122@gmail.com>
2022-05-10 16:10:49 +0800

    調整 handleError.js

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,11 +1,15 @@
 const handleError = (err, res) => {
     console.log(err);
     let error;
-    [...Object.keys(err.errors)].forEach(el =>
-        error = err.errors[el].properties?.message ?
-            { ...error, [el]: err.errors[el].properties.message } :
-            { ...error, [el]: err.errors[el].message }
-    );
+    if (err.errors) {
+        [...Object.keys(err.errors)].forEach(el =>
+            error = err.errors[el].properties?.message ?
+                { ...error, [el]: err.errors[el].properties.message } :
+                { ...error, [el]: err.errors[el].message }
+        );
+    } else {
+        error = err;
+    }
     res.status(400).json({
         "status": "failed",
         "error": error
